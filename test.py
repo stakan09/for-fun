2e15f22789303723523b9d45e14682c660da6b76
stakan09 <rnc9ndr.yg@gmail.com>
2022-05-18 23:35:56 +0900

    入力文字を返すようにした。次はこのデータをサーバーに送る。

--- test.py
+++ test.py
@@ -1,45 +1,28 @@
-# This example requires the 'message_content' intent.
-
-import discord
 from discord.ext import commands
+import discord
+import sys
+sys.path.append(".\discord_token")
+import discord_token as dscrd
 
-intents = discord.Intents.default()
-intents.message_content = True
+Token=dscrd.discToken
 
-client = discord.Client(intents=intents)
+from urllib.parse import quote_plus
 
+intents = discord.Intents.all()
 
-bot.run("OTc1MjM0NTg3NDE0Mzg4NzQ3.G-WPfP.JlhSaD82yyn4fP58KajvZgwvwgmhrvPjzNt_jA")
-
+##superは部分共通の操作を記述するときに使う。特殊の操作はsuperを呼び出した後に書く。
 class CounterBot(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix=commands.when_mentioned_or('$'))
+        super().__init__(command_prefix=commands.when_mentioned_or('##'),intents=intents)
 
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
-        channel = client.get_channel(810821992647360513)
-        view = discord.ui.View()
 
-class HogeList(discord.ui.View):
-    def __init__(self,args):
-        super().__init__()
-        self.add_item(HugaList(args))
-
-class HugaList(discord.ui.Select):
-    def __init__(self,args):
-        options=[]
-        for item in args:
-            options.append(discord.SelectOption(label=item, description=''))
-    
-        super().__init__(placeholder='', min_values=1, max_values=1, options=options)
-
-    async def callback(self, interaction: discord.Interaction):
-        await interaction.response.send_message(f'{interaction.user.name}は{self.values[0]}を選択しました')
+bot = CounterBot()
 
 @bot.command()
-async def makeButton(ctx: commands.context,*args):
-    await ctx.send('Press!', view=HogeButton(args))
+async def log(ctx, arg1,arg2,*args):
+    await ctx.send('Training:{} Weights:{} Reps:{}'.format(arg1,arg2,args))
 
-client.run('token') 
-#reset token
+bot.run(Token) 
