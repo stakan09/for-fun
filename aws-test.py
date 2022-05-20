985747e129d87bfaa9e6689e467fd19a23e2dd35
stakan09 <rnc9ndr.yg@gmail.com>
2022-05-20 23:16:47 +0900

    aws丸パクリ

---
+++ aws-test.py
@@ -0,0 +1,42 @@
+import boto3
+# snippet-start:[dynamodb.python.codeexample.MoviesCreateTable]
+class Movies:
+    """Encapsulates an Amazon DynamoDB table of movie data."""
+    def __init__(self, dyn_resource):
+        """
+        :param dyn_resource: A Boto3 DynamoDB resource.
+        """
+        self.dyn_resource = dyn_resource
+        self.table = None
+
+    def create_table(self, table_name):
+        """
+        Creates an Amazon DynamoDB table that can be used to store movie data.
+        The table uses the release year of the movie as the partition key and the
+        title as the sort key.
+
+        :param table_name: The name of the table to create.
+        :return: The newly created table.
+        """
+        try:
+            self.table = self.dyn_resource.create_table(
+                TableName=table_name,
+                KeySchema=[
+                    {'AttributeName': 'year', 'KeyType': 'HASH'},  # Partition key
+                    {'AttributeName': 'title', 'KeyType': 'RANGE'}  # Sort key
+                ],
+                AttributeDefinitions=[
+                    {'AttributeName': 'year', 'AttributeType': 'N'},
+                    {'AttributeName': 'title', 'AttributeType': 'S'}
+                ],
+                ProvisionedThroughput={'ReadCapacityUnits': 10, 'WriteCapacityUnits': 10})
+            self.table.wait_until_exists()
+        except ClientError as err:
+            logger.error(
+                "Couldn't create table %s. Here's why: %s: %s", table_name,
+                err.response['Error']['Code'], err.response['Error']['Message'])
+            raise
+        else:
+            return self.table
+
+kkk = Movies()
